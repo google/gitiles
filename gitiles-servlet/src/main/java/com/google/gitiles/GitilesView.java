1488fedb03d61924f90f6f7c9fa23ea5298dbcb9
Dave Borowitz <dborowitz@google.com>
2013-06-26 11:11:40 -0600

    Tighten up switch statements in GitilesView
    Change-Id: Ib1014b7a5a03526e7aab823be22fa7de7103c95d

--- gitiles-servlet/src/main/java/com/google/gitiles/GitilesView.java
+++ gitiles-servlet/src/main/java/com/google/gitiles/GitilesView.java
@@ -192,13 +192,12 @@ public class GitilesView {
       switch (type) {
         case DIFF:
         case LOG:
-          this.oldRevision = revision;
           break;
         default:
           checkState(revision == null, "cannot set old revision on %s view", type);
-          this.oldRevision = revision;
           break;
       }
+      this.oldRevision = revision;
       return this;
     }
 
@@ -218,18 +217,17 @@ public class GitilesView {
       switch (type) {
         case PATH:
         case DIFF:
-          this.path = maybeTrimLeadingAndTrailingSlash(checkNotNull(path));
+          checkState(path != null, "cannot set null path on %s view", type);
           break;
         case DESCRIBE:
         case REFS:
         case LOG:
-          this.path = path != null ? maybeTrimLeadingAndTrailingSlash(path) : null;
           break;
         default:
           checkState(path == null, "cannot set path on %s view", type);
-          this.path = path;
           break;
       }
+      this.path = path != null ? maybeTrimLeadingAndTrailingSlash(path) : null;
       return this;
     }
 
