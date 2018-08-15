63de61afa071426255c6a77af9771624f73be3d7
Dave Borowitz <dborowitz@google.com>
2018-08-15 14:05:55 -0700

    RevisionParserTest: Clarify comment about reflog
    Change-Id: I271cce891347a2ba4d89148c38d18ca27179d1bb

--- javatests/com/google/gitiles/RevisionParserTest.java
+++ javatests/com/google/gitiles/RevisionParserTest.java
@@ -249,7 +249,8 @@ public class RevisionParserTest {
     assertThat(repo.getRepository().resolve("master:blob")).isEqualTo(blob);
     assertThat(parser.parse("master:blob")).isNull();
 
-    // TestRepository has no simple way of setting the reflog.
+    // InMemoryRepository doesn't implement a reflog, so we can't test reflog entries.
+    // https://bugs.eclipse.org/bugs/show_bug.cgi?id=537972
     // assertThat(repo.getRepository().resolve("master@{0}")).isEqualTo(null);
     assertThat(parser.parse("master@{0}")).isNull();
   }
